2718583588008b34f92cfce1c0fa47b954890377
Evgeniy <saprunov.e@zaozet.ru>
2023-08-14 18:25:50 +0300

    hello.py page 52

---
+++ hello.py
@@ -0,0 +1,11 @@
+#
+
+print('Hello, world!')
+print('What is your name?')
+myName = input()
+print('Nice to meet you, ' + myName)
+print('Length your name: ')
+print(len(myName), ' ,latte')
+print('How old you?')
+myAge = input()
+print('In a year you will  ' + str(int(myAge) + 1) + ' year.')
